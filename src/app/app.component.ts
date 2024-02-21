5e6dc5a4dc1da47b2a8978efcaca5423d4a75107
unknown <uyerandisr@gmail.com>
2024-02-21 14:45:19 +0200

    working on section 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: 
+  [
+    CommonModule, RouterOutlet, MatButtonModule, 
+    MatDividerModule, MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
